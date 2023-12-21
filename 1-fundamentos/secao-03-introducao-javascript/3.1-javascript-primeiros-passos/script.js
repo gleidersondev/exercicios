5f8db2b7a3715e6e7963ec2323b77c5934e722cd
gleidersondavid <gleidersondev@gmail.com>
2023-12-20 22:50:38 -0300

    feito exercício 1

--- 1-fundamentos/secao-03-introducao-javascript/3.1-javascript-primeiros-passos/script.js
+++ 1-fundamentos/secao-03-introducao-javascript/3.1-javascript-primeiros-passos/script.js
@@ -1,1 +1,34 @@
-// Elabore alguns códigos para cada operação aritmética e imprima o resultado no console usando o console.log:
+// 1 - Elabore alguns códigos para cada operação aritmética e imprima o resultado no console usando o console.log:
+
+let num1 = 18;
+
+let num2 = 6;
+
+// Adição (num1 + num2)
+
+let adicao = num1 + num2;
+
+console.log(adicao);
+
+// Subtração (num1 - num2)
+
+let subtracao = num1 - num2;
+
+console.log(subtracao);
+
+// Multiplicação (num1 * num2)
+
+let multiplicacao = num1 * num2;
+
+console.log(multiplicacao);
+
+// Divisão (num1 / num2)
+
+let divisao = num1 / num2;
+console.log(divisao);
+
+// Módulo (num1 + num2)
+
+let modulo = num1 % num2
+
+console.log(modulo);
