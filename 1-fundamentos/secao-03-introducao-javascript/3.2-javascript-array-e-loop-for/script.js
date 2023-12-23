990626af463c780640bdab3c41f2cefd3f4f2f23
gleidersondavid <gleidersondev@gmail.com>
2023-12-23 17:29:03 -0300

    Feito exercicio 2-array

--- 1-fundamentos/secao-03-introducao-javascript/3.2-javascript-array-e-loop-for/script.js
+++ 1-fundamentos/secao-03-introducao-javascript/3.2-javascript-array-e-loop-for/script.js
@@ -1,6 +1,6 @@
 // Exercícios de Arrays
 
-// Altere o valor da variável menuServices para que ela passe a ter o valor "Serviços". Isso deve ser feito através da variável menu.
+// 1 - Altere o valor da variável menuServices para que ela passe a ter o valor "Serviços". Isso deve ser feito através da variável menu.
 
 const menu = ['Home', 'Serviços', 'Portifólio', 'Links']
 
@@ -8,15 +8,21 @@ const menuServices = menu[1];
 
 console.log(menuServices);
 
+// 2 - Adicione o valor 'Contato' no final do array menu
+
+menu.push('Contato');
+
+console.log(menu);
+
 
 
 // Exercícios de FOR
 
-// Utilize o FOR para imprimir os elementos da lista groceryList através do console.log
+// 1 - Utilize o FOR para imprimir os elementos da lista groceryList através do console.log
 
-const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
+// const groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia', 'Macarrão'];
 
-for (let index = 0; index < groceryList.length; index +=1) {
-  const list = groceryList[index];
-  console.log(list);
-}
+// for (let index = 0; index < groceryList.length; index +=1) {
+//   const list = groceryList[index];
+//   console.log(list);
+// }
