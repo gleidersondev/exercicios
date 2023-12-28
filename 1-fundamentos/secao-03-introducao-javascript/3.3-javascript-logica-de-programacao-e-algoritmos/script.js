98323672a90326c0d563b0c38f49830253874052
gleidersondavid <gleidersondev@gmail.com>
2023-12-27 23:38:00 -0300

    Adciona exercicios de função

---
+++ 1-fundamentos/secao-03-introducao-javascript/3.3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,8 @@
+// Desenvolva o exercicio a seguir com o uso de funções:
+// Considere a variável (balance), que representa a quantia em conta da pessoa cliente do TrybeBank e escreva quatro funções
+// que:
+
+// 1. Adiciona um valor ao balance
+// 2. Subtraia um valor do balance
+// 3. Multiplique o valor do balance por uma taxa;
+// 4. Divida o valor do balance
