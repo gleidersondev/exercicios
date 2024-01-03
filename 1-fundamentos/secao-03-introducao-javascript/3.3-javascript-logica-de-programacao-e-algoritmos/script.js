30a9e2c1343a55a9cba6cb2cd4bd774b18c1bcee
gleidersondavid <gleidersondev@gmail.com>
2024-01-02 21:48:20 -0300

    Refatora estrutura de exercicíos

--- 1-fundamentos/secao-03-introducao-javascript/3.3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ 1-fundamentos/secao-03-introducao-javascript/3.3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -1,10 +1,10 @@
-// Desenvolva o exercicio a seguir com o uso de funções:
+// 1 - Desenvolva o exercicio a seguir com o uso de funções:
 // Considere a variável (balance), que representa a quantia em conta da pessoa cliente do TrybeBank e escreva quatro funções
 // que:
 
 let balance = 50;
 
-// 1. Adiciona um valor ao balance
+// 1.1 Adiciona um valor ao balance
 
 function depositaValor(deposito) {
   balance += deposito;
@@ -13,7 +13,7 @@ function depositaValor(deposito) {
 
 console.log(depositaValor(10));
 
-// 2. Subtraia um valor do balance
+// 1.2 Subtraia um valor do balance
 
 function saqueValor(saque) {
   balance -= saque;
@@ -22,7 +22,7 @@ function saqueValor(saque) {
 
 console.log(saqueValor(6));
 
-// 3. Multiplique o valor do balance por uma taxa
+// 1.3 Multiplique o valor do balance por uma taxa
 
 function jurosRemuneratórios() {
   balance *= 1.06;
@@ -31,7 +31,7 @@ function jurosRemuneratórios() {
 
 console.log(jurosRemuneratórios());
 
-// 4. Divida o valor do balance
+// 1.4 Divida o valor do balance
 
 function divideValor(divisor) {
   balance /= divisor;
@@ -39,3 +39,10 @@ function divideValor(divisor) {
 }
 
 console.log(divideValor(2));
+
+
+// 2 - Faça um programa para adicionar pessoas clientes ao array do Trybe Bank. A função deve se chamar addCustomer e
+// receber um parâmetro do tipo string e, caso o parâmetro não seja do tipo string, imprimir a mensagem: "O parâmetro
+// passado deve ser do tipo string".
+
+const trybeBankCustomers = ['oliva', 'Nat', 'Gus'];
