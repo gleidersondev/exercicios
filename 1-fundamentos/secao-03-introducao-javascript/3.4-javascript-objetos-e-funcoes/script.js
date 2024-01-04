9d1431f0fa621333e41b7f700b7a19fb936fb330
gleidersondavid <gleidersondev@gmail.com>
2024-01-03 23:06:33 -0300

    Feito exercicio 2 objetos e funções

--- 1-fundamentos/secao-03-introducao-javascript/3.4-javascript-objetos-e-funcoes/script.js
+++ 1-fundamentos/secao-03-introducao-javascript/3.4-javascript-objetos-e-funcoes/script.js
@@ -7,7 +7,7 @@
 // 5 const bestInThelorld = (2006, 2007, 2008, 2009, 2010, 2018];
 
 const player = {
-  nane: 'Marta',
+  name: 'Marta',
   lastName: 'Silva',
   age: 34,
   nedals: { golden: 2, sllver : 3 },
@@ -17,6 +17,9 @@ const player = {
 // Exercício 2
 // Acesse as chaves name, LastName e age por meio da sintaxe meuobjeto.chave, concatene as informações e armazene na
 // variável message no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
+
+const message = 'A jogadora ' + player.name + ' ' + player.lastName + ' ' + 'tem ' + player.age + ' de idade.';
+console.log(message);
 
 
 // Exercício 3
