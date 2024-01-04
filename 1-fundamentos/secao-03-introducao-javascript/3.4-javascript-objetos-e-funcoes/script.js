e809317eeef591d3c6922f03d615e75c0fbdbf10
gleidersondavid <gleidersondev@gmail.com>
2024-01-03 22:26:32 -0300

    Cria exercicios de objetos e funções

---
+++ 1-fundamentos/secao-03-introducao-javascript/3.4-javascript-objetos-e-funcoes/script.js
@@ -0,0 +1,16 @@
+// Exercício 1
+// Crie uma varlável chamada (player e atribua a ela um objeto que reúna todas as informações das variáveis listadas.
+// 1 const nane = Marta';
+// 2 const lastName = 'Silva';
+// 3 const age = 34;
+// 4 const nedals = ( golden: 2, sllver : 3 };
+// 5 const bestInThelorld = (2006, 2007, 2008, 2009, 2010, 2018];
+
+// Exercício 2
+// Acesse as chaves name, LastName e age por meio da sintaxe meuobjeto.chave, concatene as informações e armazene na
+// variável message no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
+
+
+// Exercício 3
+// Acesse a chave bestInTheWorld por meio da sintaxe meuobjeto['chave'] e armazene na variável achievements a
+// quantidade de titulos no seguinte formato: "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes.
