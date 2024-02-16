97b057b54d1d62dfcb4194102eabe74a9e4ea78d
gleidersondavid <gleidersondev@gmail.com>
2024-02-15 23:45:03 -0300

    Refatorado exercicio 1

--- 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/main.js
+++ 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/main.js
@@ -6,8 +6,10 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 
 // 1. Copie esse arquivo e edite apenas ele.
-// 1.1. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 firstLi.classList.remove('tech');
+// 1.1. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// Por causa da propriedade tech que descoloca a li -20px no eixo y
+
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando este for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como é possível fazer isso? Dica: Lembre-se do método `.classList.remove`.
 
