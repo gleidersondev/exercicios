d7dac1ef0fbee270cd3e6d97e37ef8d4874619af
gleidersondavid <gleidersondev@gmail.com>
2024-02-25 13:07:07 -0300

    Adiciona musica ao carregar a pagina

--- 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.02/script.js
+++ 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.02/script.js
@@ -1,5 +1,6 @@
 const btnStartRace = document.getElementById('start-race');
 const carSection =document.getElementById('car-section');
+const getInitialAudio = document.querySelector('[src="./songs/initial-music.mp3"]');
 let inMotion = false;
 
 const moveRoad = () => {
@@ -18,3 +19,12 @@ const moveRoad = () => {
 }
 
 btnStartRace.addEventListener('click', moveRoad);
+
+const initialMusic = () => {
+  const audio = new Audio('./songs/initial-music.mp3');
+  audio.play();
+
+  console.log(audio);
+}
+
+window.onload = initialMusic;
