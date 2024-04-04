a369d9c7ff0676ce0962d5cdb20140064240e2da
gleidersondavid <gleidersondev@gmail.com>
2024-04-03 23:20:47 -0300

    Muda id main-calendar para tag main

--- 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
+++ 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
@@ -7,14 +7,17 @@ console.log(mainCalendar);
 
 // Lógica do calendário (calendar-content)
 
-const diasDoMes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+const diasDoMes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+const ul = document.createElement('ul');
+mainCalendar.appendChild(ul);
 
 const populaCalendario = () => {
   for (let i = 0; i < diasDoMes.length; i +=1) {
-    const div = document.createElement('div');
+    const li = document.createElement('li');
     let elemento = diasDoMes[i];
-    div.innerText = elemento;
-    mainCalendar.appendChild(div);
+    li.innerText = elemento;
+    mainCalendar.firstElementChild.appendChild(li);
   }
 }
 
