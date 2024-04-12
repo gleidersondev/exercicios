decd1e59792ebccb43f68a2d51a2242f18085810
gleidersondavid <gleidersondev@gmail.com>
2024-04-11 21:57:08 -0300

    altera estrutura html

--- 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
+++ 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
@@ -3,6 +3,7 @@ const calendarioPrincipal = document.querySelector('#main-calendar');
 const diadeHoje = document.querySelector('#todays-day');
 const calendario = document.querySelector('#calendar');
 const mesConteudoCalendario = document.querySelector('#month');
+const iniciaisDosDias = document.querySelector('#initials-of-day');
 const dataAtual = new Date();
 
 const diasDoMes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
