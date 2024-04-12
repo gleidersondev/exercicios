de99b6849cab1c7c68a7d0c38597dfd1d73a78cf
gleidersondavid <gleidersondev@gmail.com>
2024-04-11 21:40:22 -0300

    implementado função mesAtualConteudoCalendario

--- 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
+++ 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
@@ -1,6 +1,13 @@
 const conteudoCalendario = document.querySelector('#calendar-content');
-const mainCalendar = document.querySelector('#main-calendar');
-const diasDaSemana = document.querySelector('#days-of-the-week');
+const calendarioPrincipal = document.querySelector('#main-calendar');
+const diadeHoje = document.querySelector('#todays-day');
+const calendario = document.querySelector('#calendar');
+const mesConteudoCalendario = document.querySelector('#month');
+const dataAtual = new Date();
+
+const diasDoMes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
 
 // Lógica mural de compromissos (wall-content)
@@ -9,23 +16,31 @@ const diasDaSemana = document.querySelector('#days-of-the-week');
 
 // Lógica do calendário (calendar-content)
 
-const diasDoMes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const mesAtualConteudoCalendario = () => {
+  mesConteudoCalendario.textContent = meses[dataAtual.getMonth()];
+};
 
-const ul = document.createElement('ul');
-mainCalendar.appendChild(ul);
+mesAtualConteudoCalendario();
+
+
+const ul = document.createElement('ul'); 
+calendario.appendChild(ul);
+
 
 const populaCalendario = () => {
   for (let i = 0; i < diasDoMes.length; i +=1) {
     const li = document.createElement('li');
     let elemento = diasDoMes[i];
     li.innerText = elemento;
-    mainCalendar.firstElementChild.appendChild(li);
+    calendario.firstElementChild.appendChild(li);
     li.classList.add('.day');
-    console.log(li);
   }
 }
 
 populaCalendario();
+
+console.log(dataAtual.getMonth() + 1);
+
 
 
 
