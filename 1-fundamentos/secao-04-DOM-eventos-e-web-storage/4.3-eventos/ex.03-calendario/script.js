33010c6f500122450d00795cf8e02c5a43d346fc
gleidersondavid <gleidersondev@gmail.com>
2024-04-21 18:29:18 -0300

    Fazendo logica para colocar o dia do mes no calendario  conf. dia atual

--- 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
+++ 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
@@ -61,7 +61,14 @@ const populaCalendario = () => {
     // div.textContent = i;
     for (let i = diaDaSemana; i < diasDoMes.length; i +=1) {
       const item = document.querySelector(`#item-${i}`);
-      const element = array[i];
+    // fazer logica para textContent item
+      for (let c = 0; c < diasDoMes.length; c +=1) {
+        if (c <= diasDoMes) {
+          
+        }
+        
+      }
+      item.textContent = diasDoMes[]
       
     }
   }
