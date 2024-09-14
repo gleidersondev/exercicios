72fd77a201c8c327d8a5eeb537586840e2c20d51
gleidersondavid <gleidersondev@gmail.com>
2024-09-14 15:39:33 -0300

    Altera variavel dataAtual de const para let

--- 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
+++ 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
@@ -4,13 +4,11 @@ const diadeHoje = document.querySelector('#todays-day');
 const calendario = document.querySelector('#calendar');
 const mesConteudoCalendario = document.querySelector('#month');
 const iniciaisDosDias = document.querySelector('#initials-of-day');
-const dataAtual = new Date();
+let dataAtual = new Date();
 const retrocederMesCalendarioPrincipal = conteudoCalendario.querySelector('#go-back-month');
 
 // let retroceder = uma função que altere a dataAtual.getMonth para o mes anterior e altere automaticamnte a função encontrarPrimeiroDiaDoMes dataAtual.setDate(1)
 const mesAtivo = dataAtual.getMonth();
-console.log(typeof mesAtivo);
-
 
 const diasDoMes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
