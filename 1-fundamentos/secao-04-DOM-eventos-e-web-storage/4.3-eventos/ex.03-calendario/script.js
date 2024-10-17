98aa07fbb7593c4110daec75166710bd76512364
gleidersondavid <gleidersondev@gmail.com>
2024-10-16 22:29:57 -0300

    Implementada a função retrocederPostIt

--- 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
+++ 1-fundamentos/secao-04-DOM-eventos-e-web-storage/4.3-eventos/ex.03-calendario/script.js
@@ -49,9 +49,20 @@ console.log('primeiroDiaDaSemana é', primeiroDiaDaSemana);
 // LÓGICA MURAL DE COMPROMISSOS (WALL-CONTENT)
 mesAtualConteudoPostIt.textContent = meses[dataAtual.getMonth()]; //Preenche mês ativo no mural de compromissos
 
+let mesAtivoMuralCompromissos = dataAtual.getMonth();
+console.log('O mes ativo no mural de compromisso e *****', mesAtivoMuralCompromissos);
+
 const retrocederPostIt = () => {
-  mesAtualConteudoPostIt.textContent = '';
-  mesAtualConteudoPostIt.textContent = meses[dataAtual.getMonth() - 1];
+
+  if (mesAtivoMuralCompromissos < 1) {
+    mesAtivoMuralCompromissos = 11;
+    mesAtualConteudoPostIt.textContent = meses[11];
+  } else {
+    mesAtualConteudoPostIt.textContent = meses[mesAtivoMuralCompromissos - 1];
+    mesAtivoMuralCompromissos -= 1; 
+    console.log(mesAtivoMuralCompromissos);
+    
+  }
 };
 
 retrocederMesMuralCompromissos.addEventListener('click', retrocederPostIt);
